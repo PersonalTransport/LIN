0296eb146621526e28b869b721b60029a2dd9667
Aaron Siegel <siegelaaron94@gmail.com>
2015-12-02 12:06:36 -0800

    Started the command line interface for the compiler.

--- src/LIN2/compiler/Compiler.java
+++ src/LIN2/compiler/Compiler.java
@@ -2,6 +2,8 @@ package LIN2.compiler;
 
 
 import LIN2.Cluster;
+import LIN2.Node;
+import LIN2.Slave;
 import LIN2.compiler.generation.models.*;
 import LIN2.compiler.parser.description.ClusterVisitor;
 import LIN2.compiler.parser.description.DescriptionFileLexer;
@@ -19,6 +21,7 @@ import org.stringtemplate.v4.STGroupFile;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Compiler {
     private static void addModelAdaptors(STGroup group) {
@@ -30,7 +33,41 @@ public class Compiler {
         group.registerModelAdaptor(EncodedValue.class,new EncodedValueModelAdaptor());
     }
 
+    public static void helpMessage() {
+        System.out.println("Usage: jLIN [options] files...");
+        System.out.println();
+        System.out.println("Options:");
+        System.out.println("  -h                      Output this help message.");
+        System.out.println("  -o                      Output location.");
+        System.out.println("  -driver                 Generated the LIN2 C driver.");
+    }
+
     public static void main(String [] args) throws IOException {
+        ArrayList<String> sourceFiles = new ArrayList<>();
+        if(args.length == 0) {
+            helpMessage();
+            return;
+        }
+        for(String arg:args) {
+            if(arg.equals("-h")) {
+                helpMessage();
+                return;
+            }
+            else if(arg.equals("-o")) {
+            }
+            else if(arg.equals("-driver")) {
+
+            }
+            else if(arg.startsWith("-")) {
+                System.err.println("Unreco");
+                return;
+            }
+            else {
+                sourceFiles.add(arg);
+            }
+        }
+
+        /*
         for(String arg : args) {
             System.out.println(arg);
 
@@ -45,24 +82,32 @@ public class Compiler {
                 STGroup capabilityFileGroup = new STGroupFile("LIN2/compiler/generation/template/CapabilityFile.stg");
                 addModelAdaptors(capabilityFileGroup);
 
+                STGroup slaveDriverHeader = new STGroupFile("LIN2/compiler/generation/template/SlaveDriverHeader.stg");
+                addModelAdaptors(slaveDriverHeader);
+
+                STGroup slaveDriverSource = new STGroupFile("LIN2/compiler/generation/template/SlaveDriverSource.stg");
+                addModelAdaptors(slaveDriverSource);
+
+                Slave slave = cluster.getSlave("LSM");
+
                 System.out.println("//-------------------------------------------------------------------------------------------------------------//");
                 ST capabilityFile = capabilityFileGroup.getInstanceOf("capabilityFile");
-                capabilityFile.add("slave",cluster.getSlave("LSM"));
+                capabilityFile.add("slave",slave);
                 System.out.println(capabilityFile.render());
                 System.out.println("//-------------------------------------------------------------------------------------------------------------//");
 
-                /*System.out.println("//-------------------------------------------------------------------------------------------------------------//");
-                ST slaveHeader = group.getInstanceOf("slaveDriverHeader");
-                slaveHeader.add("slave",cluster.getSlave("LSM"));
+                System.out.println("//-------------------------------------------------------------------------------------------------------------//");
+                ST slaveHeader = slaveDriverHeader.getInstanceOf("slaveDriverHeader");
+                slaveHeader.add("slave",slave);
                 System.out.println(slaveHeader.render());
                 System.out.println("//-------------------------------------------------------------------------------------------------------------//");
 
                 System.out.println("//-------------------------------------------------------------------------------------------------------------//");
-                ST slaveSource = group.getInstanceOf("slaveDriverSource");
-                slaveSource.add("slave",cluster.getSlave("LSM"));
+                ST slaveSource = slaveDriverSource.getInstanceOf("slaveDriverSource");
+                slaveSource.add("slave",slave);
                 System.out.println(slaveSource.render());
-                System.out.println("//-------------------------------------------------------------------------------------------------------------//");*/
+                System.out.println("//-------------------------------------------------------------------------------------------------------------//");
             }
-        }
+        }*/
     }
 }
