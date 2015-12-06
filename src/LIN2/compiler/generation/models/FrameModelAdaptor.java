822890a8ceb52eaf1aac0949cc6f8568cf7be8a6
Aaron Siegel <siegelaaron94@gmail.com>
2015-12-05 21:05:22 -0800

    Fixed a bug in the FrameModelAdaptor.

--- src/LIN2/compiler/generation/models/FrameModelAdaptor.java
+++ src/LIN2/compiler/generation/models/FrameModelAdaptor.java
@@ -12,9 +12,9 @@ import org.stringtemplate.v4.misc.STNoSuchPropertyException;
 public class FrameModelAdaptor extends ObjectModelAdaptor {
     @Override
     public synchronized Object getProperty(Interpreter interp, ST self, Object o, Object property, String propertyName) throws STNoSuchPropertyException {
-        Object slaveObj = self.getAttribute("slave");
-        if(propertyName.equals("publishedBy") && slaveObj != null && slaveObj instanceof Slave)
-            return ((Frame)o).publishedBy((Node)slaveObj);
+        Object nodeObj = self.getAttribute("slave");
+        if(propertyName.equals("publishedBy") && nodeObj != null && nodeObj instanceof Node)
+            return ((Frame)o).publishedBy((Node)nodeObj);
         return super.getProperty(interp, self, o, property, propertyName);
     }
 }
